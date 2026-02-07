623ca6938fc2bd6f877f208b5c97b95dad957cfa
sante <musa62298@gmail.com>
2026-02-07 19:01:43 +0300

    Initial pipeline-executor

---
+++ services/pipeline-executor/src/main/java/com/example/pipelineexecutor/PipelineExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipelineexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineExecutorApplication.class, args);
+    }
+
+}
